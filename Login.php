842cf268e143d5fa3b0107def14ea9b8ad1443d3
simobls <belassalsimo78@gmail.com>
2023-11-19 17:11:12 +0100

    add

--- Login.php
+++ Login.php
@@ -7,9 +7,32 @@
     <link rel="stylesheet" href="css/login.css">
 </head>
 <body>
+    <?php
+        if(isset($_POST["submit"])){
+            $emailU=$_POST['email'];
+            $passU=$_POST['password'];
+            $erreur="";
+    
+            $cnx=mysqli_connect('localhost','root','123');
+            $db_select=mysqli_select_db($cnx,'GESTION_STOCK');
+            
+            $req="SELECT * FROM Utilisateurs where email='$emailU' and password='$passU'";
+            $res=mysqli_query($cnx,$req);
+            if(mysqli_num_rows($res)>0){
+                header("Location:bienvenue.php");
+            }
+            else $erreur="Email ou mot de passe incorrect";
+        }
+    ?>
     <section class="cont">
         <div class="formul">
             <p class="welcome">Bienvenue, Admin.</p>
+            <img src="assets/3515462.jpg" alt="">
+            <?php
+                if(isset($erreur)){
+                    echo "<p class='erreur'>".$erreur."</p>";
+                }
+            ?>
             <form method="post">
                 <input type="text" name="user" required="required">
                 <label class="text">Nom d'utilisateur</label><br>
@@ -22,13 +45,5 @@
             <img src="assets/3515462.jpg" alt="">
         </div>
     </section>
-    <?php
-        if(isset($_POST["submit"])){
-            $user = $_POST["user"];
-            $pwd = $_POST["pwd"];
-            $file = fopen("data.txt", "a+");
-            fwrite($file, $user . "/" . $pwd);
-        }
-    ?>
 </body>
 </html>
